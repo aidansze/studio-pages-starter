2ac7a9249496a59341fd13bcbcb8d953f049eea2
Aidan Sze <asze@yext.com>
2023-07-31 12:10:57 -0400

    removed card

--- src/templates/Liberty.tsx
+++ src/templates/Liberty.tsx
@@ -1,6 +1,4 @@
-import { Card } from "@nextui-org/react";
 import { GetPath, TemplateProps } from "@yext/pages";
-import HStack from "../components/HStack";
 import LibertyMutualNavigationBar from "../components/LibertyMutualNavigationBar";
 
 export const getPath: GetPath<TemplateProps> = () => {
@@ -9,30 +7,15 @@ export const getPath: GetPath<TemplateProps> = () => {
 
 export default function Liberty() {
   return (
-    <>
-      <HStack space="1">
-        <Card
-          title="External Asset Fetched at Build Time"
-          titleFontSize="text-xl"
-          titleFontWeight="font-bold"
-          titleTextColor="text-gray-900"
-          subtitle="Default subtitle"
-          subtitleFontSize="text-base"
-          subtitleFontWeight="font-normal"
-          subtitleTextColor="text-gray-700"
-          url=""
-        />
-      </HStack>
-      <LibertyMutualNavigationBar
-        logoSrc={`https://1000logos.net/wp-content/uploads/2020/09/Liberty-Mutual-Logo.png`}
-        links={[
-          { text: "Shop insurance", url: "/Shop" },
-          { text: "Claims", url: "/Claims" },
-          { text: "Customer Support", url: "/CustomerSupport" },
-        ]}
-        fontWeight="font-normal"
-        textColor="blue-900"
-      />
-    </>
+    <LibertyMutualNavigationBar
+      logoSrc={`https://1000logos.net/wp-content/uploads/2020/09/Liberty-Mutual-Logo.png`}
+      links={[
+        { text: "Shop insurance", url: "/Shop" },
+        { text: "Claims", url: "/Claims" },
+        { text: "Customer Support", url: "/CustomerSupport" },
+      ]}
+      fontWeight="font-normal"
+      textColor="blue-900"
+    />
   );
 }
